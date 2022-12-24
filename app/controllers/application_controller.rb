c24d4cc0064e1edd811cd555875a69f37efff75c
Daniel Clements <drclements@liberty.edu>
2022-12-23 22:29:01 -0500

    commit tree 3a5786805cb86b24b65408e9c8d3fd0a34d85c6b

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,36 @@
 class ApplicationController < Sinatra::Base
 
+  set :default_content_type, 'application/json'
   # add routes
+
+  get '/bakeries' do 
+    #get all the bakeries from the database
+    bakeries = Bakery.all
+    #send them back as JSON array
+    bakeries.to_json
+  end
+
+  get "/bakeries/:id" do 
+    bakery = Bakery.find(params[:id])
+
+    bakery.to_json(include: :baked_goods)
+  end
+
+  get "/baked_goods" do 
+    baked_goods = BakedGood.all
+    baked_goods.to_json
+  end
+
+  get "/baked_goods/by_price" do
+  baked_goods = BakedGood.by_price
+
+  baked_goods.to_json
+  end
+
+  get "/baked_goods/most_expensive" do 
+    baked_goods = BakedGood.by_price.most_expensive
+
+    baked_goods.to_json
+  end
 
 end
